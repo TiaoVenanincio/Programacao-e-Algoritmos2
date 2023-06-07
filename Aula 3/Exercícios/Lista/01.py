b6dc6863d664382e6abebb7179fa0627dec5bd4d
sebastiao.neto <sebastiao.neto@catijr.com.br>
2023-06-07 16:46:13 -0300

    Add solucao ex1 da lista

--- Aula 3/Exercícios/Lista/01.py
+++ Aula 3/Exercícios/Lista/01.py
@@ -1,3 +1,19 @@
 # Faça um programa que leia n nomes inserindo-os em uma lista de forma ordenada
 #utilizando a ideia do algoritmo da inserção. No final, o programa deve mostrar todos os nomes
 #ordenados alfabeticamente.
+
+nomes = ['Sebastiao','Yago','Maria','Ana','Valter']
+
+indice_minimo =  nomes[0][0]
+for i in range(1,len(nomes)):
+    while i > 0 and nomes[i-1][0] > nomes[i][0]:
+    #Desloca os itens da esqueda para direta e, no espaço que sobra na direita(j),
+    #insere o menor número
+        aux = nomes[i]
+        nomes[i] = nomes[i-1]
+        nomes[i-1] = aux
+        i = i - 1
+
+print(nomes)
+
+
