0596ed1fdb9788795a1152f3a21fdd61a78886d5
sebastiao.neto <sebastiao.neto@catijr.com.br>
2023-07-26 18:16:07 -0300

    class lista

--- Tópico 4/Exercícios/lista.py
+++ Tópico 4/Exercícios/lista.py
@@ -20,3 +20,40 @@ class Lista:
         while atual != None:
             print(atual.data)
             atual = atual.next
+
+    def size(self):
+        atual = self.head
+        count = 0
+        while atual is not None:
+            count += 1
+            atual = atual.next
+        return count
+    
+    def append(self, valor):
+        novo = Node(valor)
+        novo.next = None
+        if self.isEmpty()==True:
+            self.head = novo
+        else:
+            atual = self.head
+            while atual.next is not None:
+                atual = atual.next
+            atual.next = novo
+
+    def pop(self):
+        if self.isEmpty()==True:
+            print("Não foi possivel remover o item. A lista está vazia")
+            return None
+        else:
+            atual = self.head
+            anterior = None
+            while atual.next is not None:
+                anterior = atual
+                atual = atual.next
+
+            if anterior is None:
+                self.head = None
+            else:
+                anterior.next = None
+            return atual.data
+                
